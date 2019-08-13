8b9224b594660c4c1a888bc9fa9f79279530dabc
github username <github email address>
2019-08-12 23:23:42 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({filterByType, filterByPrice, filterByTicker, checkedTicker, checkedPrice}) => {
+  console.log(checkedTicker)
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={checkedTicker} onClick={filterByTicker}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={checkedPrice} onClick={filterByPrice}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={filterByType}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
